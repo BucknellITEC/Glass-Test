59fd3a9c99501cc64175519846b15bd24e54448e
Andrew Famiglietti <afamiglietti@gmail.com>
2013-10-18 14:39:25 -0400

    image url fix

--- index.php
+++ index.php
@@ -68,7 +68,7 @@ switch ($_POST['operation']) {
       $second_timeline_item->setText("Bertrand Library After Fire");
       $second_timeline_item->setBundleId("Bucknell_Tour");
       $image_url =  "http://sandboxitec.net/Glass-Test/static/images/chipotle-tube-640x360.jpg";
-      insert_timeline_item($mirror_service, $second_timeline_item, 'image/jpeg', $image_url);
+      insert_timeline_item($mirror_service, $second_timeline_item, 'image/jpeg', file_get_contents($image_url));
    
     $message = "Timeline Item inserted!";
     break;
